c80546602c89b5efbcef556b347cdaa1ec9a500c
IgnacioDL55 <i.deluca@alumno.um.edu.ar>
2023-09-20 08:15:09 -0300

    Finalizado

---
+++ persona.py
@@ -0,0 +1,10 @@
+class MenorEdadException(Exception):
+    pass
+
+class Persona:
+    def __init__(self,nombre,apellido,edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        if edad < 18:
+            raise MenorEdadException()
